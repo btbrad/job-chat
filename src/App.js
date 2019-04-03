e1bc3d7d1b3de87c92a689cbf0eac275d1143abc
btbrad <biantaobrad@gmail.com>
2019-04-04 00:32:30 +0800

    修改readme

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+/*
+ * @Author: btbrad
+ * @LastEditors: btbrad
+ * @Description: 
+ * @Date: 2019-04-04 00:10:36
+ * @LastEditTime: 2019-04-04 00:27:40
+ */
 import React, { Component } from 'react';
+import { Button } from 'antd-mobile'
 
 export default class App extends Component{
   render(){
     return (
-      <h2>App组件</h2>
+      <div>
+        <h2>App组件</h2>
+        <Button>hello</Button>
+      </div>
     )
   }
 };
