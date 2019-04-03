aeb2428352bceeead28430033f0a72fd4abd2523
btbrad <biantaobrad@gmail.com>
2019-04-04 00:23:52 +0800

    first commit

---
+++ src/config.js
@@ -0,0 +1,8 @@
+/*
+ * @Author: btbrad
+ * @LastEditors: btbrad
+ * @Description: 
+ * @Date: 2019-04-04 00:11:54
+ * @LastEditTime: 2019-04-04 00:15:30
+ */
+import axios from 'axios'
---
+++ src/index.js
@@ -0,0 +1,16 @@
+/*
+ * @Author: btbrad
+ * @LastEditors: btbrad
+ * @Description: 
+ * @Date: 2019-04-03 23:51:38
+ * @LastEditTime: 2019-04-04 00:11:36
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App'
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
